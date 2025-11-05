a64f5fc54fef82ae6df597a288b9d2ce4755c2fd
Ambika School <ambikaschool@gmail.com>
2025-11-05 02:57:54 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function oddoreven(a,b)
+{
+    if ((a+b) % 2==0)
+    {
+        console.log("even")
+    }
+else{
+    console.log("odd")
+}
+    }
+    
---
+++ number.js
@@ -0,0 +1,15 @@
+function whichGreater(num1, num2, num3,){
+    if (num1>num2 && num3)
+    {
+        console.log("num1 is greater");
+    }
+    else {
+        console.log("num2 is greater");
+    }
+    else 
+    {
+        console.log("num3 is greater");
+    }
+    
+    isGreater(num2,num3)
+}
