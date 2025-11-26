09305104d6ec23ba23db78b11844ca529dc2d2b2
Ambika School <ambikaschool@gmail.com>
2025-11-26 02:27:34 -0800

    second commit

---
+++ arrow.js
@@ -0,0 +1,12 @@
+const sum =(a,b) => 
+{
+    if (a>b)
+    {
+        console.log("a is greater");
+
+    }
+    else {
+        console.log("b is greater");
+    }
+}
+sum(2,3)
---
+++ object.js
@@ -0,0 +1,7 @@
+const person = {
+    name : "Kale vai",
+    age : "99",
+    hobbies : ["sutni,khani"]
+
+}
+console.log(person.name);
